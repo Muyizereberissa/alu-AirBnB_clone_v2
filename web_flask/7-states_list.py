1bfaa8f636fbb83e26c3f3971aaba3fee5d40820
nasimwe <n.asimwe@alustudent.com>
2024-06-15 10:38:24 +0000

    web

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -22,5 +22,8 @@ def teardown(self):
     """Remove the current SQLAlchemy Session"""
     storage.close()
 
-
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
 
